fe89081ec668b7cdb6ddf1a7ea5aa1f4addd7389
naveed-rana <rana.naveed812@gmail.com>
2023-07-17 22:17:07 +0500

    fix

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// var a = 23;
+// var sf = 23.234;
+
+// var b = "asfd"
+// var c = 'saf'
+// var d = `etdsg`
+
+// var asf = true
+
+
+// var $_sfsdf3242 = 21;
+
+// document.write(a)
+
+// alert(a)
+
+// console.log(a);
+
+
+
+
+var num1 = 10;
+var num2 = 20;
+
+var result = num1 - num2 + 10 * 2;
+
+console.log(result);
